025595c3f1c10d4fbf929383c80220568fc60855
McKay Mower <mmower777@gmail.com>
2021-06-10 00:49:40 -0600

    todo: figure out ri,ci

--- visualizer/src/components/Board.jsx
+++ visualizer/src/components/Board.jsx
@@ -1,59 +1,99 @@
 import { useState, useEffect } from 'react'
 import '../css for components/Board.css'
 
-const createBoard = (rowCount, colCount) => {
-    let board = [];
-    for (let x = 0; x < rowCount; x++) {
-        let col = [];
-        for (let y = 0; y < colCount; y++) {
-            col.push({
-                x: x,
-                y: y,
-                visited: false,
-                isWall: false,
-                isStart: false,
-                isStop: false,
-                distance: Infinity,
-                prev: null,
-            });
-        }
-        board.push(col);
-    }
-    return board
-}
+const Board = ({ clearValue }) => {
 
-//eat piss
-
-let walls = []
-
-const handleOnClick = (event, x, y) => {
-    if(event.target.style.backgroundColor === 'white'){
-        event.target.style.backgroundColor = 'black'
-        walls.push({x,y})
-        console.log(walls)
-    }
-    else {
-        event.target.style.backgroundColor = 'white'
-        walls = walls.filter(wall => wall !== {x,y})
-        console.log(walls)
-    }
-}
-const Board = ({clearValue}) => {
     const [board, setBoard] = useState([])
-    const [clicking, setClicking] = useState(false)
-    //const [clearValue, setClearValue] = useState(false)
+    const [clickingCell, setClickingCell] = useState(false)
+    const [clickingStart, setClickingStart] = useState(false)
+    const [clickingStop, setClickingStop] = useState(false)
+    const [startCoordinates, setStartCoordinates] = useState({ row: 15, col: 14 })
+    const [stopCoordinates, setStopCoordinates] = useState({ row: 15, col: 42 })
+
+    const createBoard = (rowCount, colCount) => {
+        let board = [];
+        for (let x = 0; x < rowCount; x++) {
+            let col = [];
+            for (let y = 0; y < colCount; y++) {
+                col.push({
+                    x: x,
+                    y: y,
+                    visited: false,
+                    isWall: false,
+                    isStart: false,
+                    isStop: false,
+                    distance: Infinity,
+                    prev: null,
+                });
+            }
+            board.push(col);
+        }
+        return board
+    }
+
+    const handleMouseOver = (event, ci, ri) => {
+        if (clickingCell) {
+            if (event.target.className === 'start-cell' || event.target.className === 'stop-cell') { }
+            else {
+                event.target.style.backgroundColor === 'white' ?
+                    event.target.style.backgroundColor = 'black' : event.target.style.backgroundColor = 'white'
+            }
+        }
+        else if (clickingStart) {
+            if (event.target.className === 'stop-cell') { }
+            else {
+                event.target.style.backgroundColor = 'green'
+                event.target.className = 'start-cell'
+                setStartCoordinates({ ri, ci })
+            }
+        }
+        else if (clickingStop) {
+            if (event.target.className === 'start-cell') { }
+            else {
+                event.target.style.backgroundColor = 'red'
+                event.target.className = 'stop-cell'
+                setStopCoordinates({ ri, ci })
+            }
+        }
+        else { }
+    }
+
+    const handleSSLeave = (event, ci, ri) => {
+        if (clickingStart) {
+            event.target.className = 'cell'
+            event.target.style.backgroundColor = 'white'
+            console.log(startCoordinates);
+        }
+        else if (clickingStop) {
+            event.target.className = 'cell'
+            event.target.style.backgroundColor = 'white'
+            console.log(stopCoordinates);
+        }
+    }
 
     //creates new board
-    useEffect(() =>{
-        console.log('CLEARED BOARD');
-        setBoard(createBoard(29,56))
-    }, [clearValue])
+    useEffect(() => {
+        setBoard(createBoard(29, 56))
+    }, [])
 
     let style = {
         width: '25px',
         height: '25px',
         border: '0.5px solid black',
         backgroundColor: 'white',
+    }
+
+    let startStyle = {
+        width: '25px',
+        height: '25px',
+        border: '0.5px solid black',
+        backgroundColor: 'green',
+    }
+    let stopStyle = {
+        width: '25px',
+        height: '25px',
+        border: '0.5px solid black',
+        backgroundColor: 'red',
     }
 
     return (
@@ -63,21 +103,71 @@ const Board = ({clearValue}) => {
                     return (
                         <tr className='row' key={ri} >
                             {row.map((col, ci) => {
-                                return (
-                                    <td className='cell'
-                                        style={style}
-                                        key={ri + '-' + ci}
-                                        onDragStart={(event) => { event.preventDefault() }}
-                                        onPointerDown={(event) => {
-                                            setClicking(true)
-                                            handleOnClick(event, ci, ri)
-                                        }}
-                                        onPointerUp={() => setClicking(false)}
-                                        onMouseOver={(event) => {
-                                            clicking && handleOnClick(event, ci, ri)
-                                        }}
-                                    >
-                                    </td>)
+                                if (ri === startCoordinates.row && ci === startCoordinates.col) {
+                                    return (
+                                        <td className='start-cell'
+                                            style={startStyle}
+                                            key={ri + '-' + ci}
+                                            onDragStart={(event) => { event.preventDefault() }}
+                                            onMouseDown={(event) => {
+                                                setClickingStart(true)
+                                                setClickingCell(false)
+                                                setClickingStop(false)
+                                            }}
+                                            onMouseUp={() => {
+                                                setClickingCell(false)
+                                                setClickingStart(false)
+                                                setClickingStop(false)
+                                            }}
+
+                                            onMouseOut={(event) => { handleSSLeave(event, ci, ri) }}
+                                            onMouseOver={(event) => { handleMouseOver(event, ci, ri) }}
+                                        >
+                                        </td>)
+                                }
+                                else if (ri === stopCoordinates.row && ci === stopCoordinates.col) {
+                                    return (
+                                        <td className='stop-cell'
+                                            style={stopStyle}
+                                            key={ri + '-' + ci}
+                                            onDragStart={(event) => { event.preventDefault() }}
+                                            onMouseDown={(event) => {
+                                                setClickingStop(true)
+                                                setClickingStart(false)
+                                                setClickingCell(false)
+                                            }}
+                                            onMouseUp={() => {
+                                                setClickingCell(false)
+                                                setClickingStart(false)
+                                                setClickingStop(false)
+                                            }}
+                                            onMouseOut={(event) => { handleSSLeave(event, ci, ri) }}
+                                            onMouseOver={(event) => { handleMouseOver(event, ci, ri) }}
+                                        >
+                                        </td>)
+
+                                }
+                                else {
+                                    return (
+                                        <td className='cell'
+                                            style={style}
+                                            key={ri + '-' + ci}
+                                            onDragStart={(event) => { event.preventDefault() }}
+                                            onMouseDown={(event) => {
+                                                setClickingCell(true)
+                                                setClickingStart(false)
+                                                setClickingStop(false)
+                                            }}
+                                            onMouseUp={() => {
+                                                setClickingCell(false)
+                                                setClickingStart(false)
+                                                setClickingStop(false)
+                                            }}
+                                            onMouseOut={(event) => { handleSSLeave(event, ci, ri) }}
+                                            onMouseOver={(event) => { handleMouseOver(event, ci, ri) }}
+                                        >
+                                        </td>)
+                                }
                             })}
                         </tr>
                     )
